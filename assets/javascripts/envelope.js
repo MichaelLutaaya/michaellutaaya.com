5eb93124642461964a73d9bb091d3d5cb41f7fd2
Michael Lutaaya <michael@lutaaya.com>
2017-05-05 12:24:36 -0400

    Use external Javascript instead
    This leads to a safer Content Security Policy

---
+++ assets/javascripts/envelope.js
@@ -0,0 +1,1 @@
+document.write("<n uers=\"znvygb:zvpunry@yhgnnln.pbz\"><fcna pynff=\"rznvy\" nevn-ynory=\"Fraq na rznvy gb Zvpunry Yhgnnln\" gvgyr=\"Fraq na rznvy gb Zvpunry Yhgnnln\"></fcna></n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26)}));
